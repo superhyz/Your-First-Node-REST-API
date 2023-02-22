d92678ca9e4470159a6b82253ff8194b14c974bd
Heng Yang Zhi <superhyz123@gmail.com>
2023-02-23 02:58:35 +0800

    updaytemongoose

--- server.js
+++ server.js
@@ -4,10 +4,24 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open', () => console.log('Connected to Database'))
+
+mongoose.connect("mongodb://0.0.0.0:27017/Subscriberdb") //0.0.0.0:27017 new version , old localhost:27017
+.then(() => {
+    console.log("MONGO CONNECTION OPEN!!!")
+})
+.catch(err => {
+    console.log("OH NO MONGO CONNECTION ERROR!!!!")
+    console.log(err)
+})
+
+app.listen(4000,()=>{
+    console.log("Server is listening on port 4000")
+})
+
 
 app.use(express.json())
 
